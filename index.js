557815a583d9c3e4edccad07b6669fa3d771691b
tygoAlfe <81092343+tygoAlfe@users.noreply.github.com>
2021-05-20 17:22:29 +0200

    First

---
+++ index.js
@@ -0,0 +1,87 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({partials: ["MESSAGE", "USER", "REACTION"]});
+const enmap = require('enmap');
+const {token, prefix} = require('./config.json');
+const fs = require('fs')
+
+const settings = new enmap({
+    name: "settings",
+    autoFetch: true,
+    cloneLevel: "deep",
+    fetchAll: true
+});
+
+client.on('ready', () => {
+    console.log(`${client.user.username} is online`);
+	client.user.setActivity(`#ticket-support`, { type: 'WATCHING' })
+});
+
+client.on('message', async message => {
+    if(message.author.bot) return;
+    if(message.content.indexOf(prefix) !== 0) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    if(command == "ticket-setup") {
+        // ticket-setup #channel
+
+        let channel = message.mentions.channels.first();
+        if(!channel) return message.reply("Usage: `!ticket-setup #channel`");
+
+        let sent = await channel.send(new Discord.MessageEmbed()
+            .setTitle("Ticket")
+            .setDescription("Kilk op het emoji om een Ticket te openen!")
+            .setFooter("Ticket System")
+            .setColor("9C0412")
+        );
+
+        sent.react('🎫');
+        settings.set(`${message.guild.id}-ticket`, sent.id);
+
+        message.channel.send("Ticket gestuurt!")
+    }
+
+    if(command == "close") {
+        if(!message.channel.name.includes("ticket-")) return message.channel.send("Dat kan niet hier!")
+        message.channel.delete();
+    }
+
+});
+
+client.on('messageReactionAdd', async (reaction, user) => {
+    if(user.partial) await user.fetch();
+    if(reaction.partial) await reaction.fetch();
+    if(reaction.message.partial) await reaction.message.fetch();
+
+    if(user.bot) return;
+
+    let ticketid = await settings.get(`${reaction.message.guild.id}-ticket`);
+
+    if(!ticketid) return;
+
+    if(reaction.message.id == ticketid && reaction.emoji.name == '🎫') {
+        reaction.users.remove(user);
+
+        reaction.message.guild.channels.create(`ticket-${user.username}`, {
+            permissionOverwrites: [
+               
+                {
+                    id: user.id,
+                    allow: ["SEND_MESSAGES", "VIEW_CHANNEL"]
+                },
+                {
+                    id: reaction.message.guild.roles.everyone,
+                    deny: ["VIEW_CHANNEL"]
+                },
+               
+            ],
+            type: 'text'
+        }).then(async channel => {
+            channel.send(`<@${user.id}>`, new Discord.MessageEmbed().setTitle("Welkom bij u ticket!").setDescription("We zijn zo bij je").setColor("00ff00"))
+            
+        })
+    }
+});
+
+client.login(token);
